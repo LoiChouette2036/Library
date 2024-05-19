99fb00b462ce80525386aae7982a3af288d8b402
LoiChouette2036 <tristanbellanger10@gmail.com>
2024-05-19 23:42:04 +0900

    Updated scrip.js

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const myLibrary = [];
 
 //the constructor
-function Book() {
-
+function Book (title,author,pages,read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return "The " + title + " by " + author +", " + pages + " pages, " + read ;
+    }
 }
 
-function addBookTolibrary {
-    
+//test of the constructor 
+const book1 = new Book('empire of north','kawagaha mahito', 235, "not read");
+console.log(book1.info());
+
+function addBookTolibrary(){
 }
